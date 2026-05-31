7a6e45e8e1d5601a188fb9ed9176ee63fda51cea
satriaego <satriaegovania04@gmail.com>
2026-05-31 16:00:05 +0700

    pertama

---
+++ modelling.py
@@ -0,0 +1,61 @@
+import os
+import argparse
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import mlflow
+import mlflow.sklearn
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_estimators", type=int, default=100)
+parser.add_argument("--max_depth", type=int, default=5)
+args = parser.parse_args()
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+if os.environ.get("GITHUB_ACTIONS") == "true":
+    import dagshub
+    print("🌐 Mendeteksi GitHub Actions, mengalihkan tracking ke DagsHub Cloud...")
+    dagshub.init(
+        repo_owner='satriaego',
+        repo_name='Kriteria-2-Membangun-Model-Machine-Learning',
+        mlflow=True
+    )
+else:
+    print("💻 Menjalankan secara lokal di komputer...")
+    mlflow.set_tracking_uri(f"file:///{os.path.join(current_dir, 'mlruns')}")
+
+mlflow.sklearn.autolog()
+
+mlflow.set_experiment("Eksperimen_SML_Satria_Ego_Vania")
+
+print(f"🚀 Memulai latihan model dengan n_estimators={args.n_estimators}, max_depth={args.max_depth}...")
+
+with mlflow.start_run():
+
+    data_path = os.path.join(current_dir, 'preprocessing', 'Titanic_cleaned_latest.csv')
+    
+    if not os.path.exists(data_path):
+        raise FileNotFoundError(f"❌ Waduh, file data tidak ditemukan di: {data_path}")
+        
+    df = pd.read_csv(data_path)
+    df_numeric = df.select_dtypes(include=['int64', 'float64'])
+    
+    X = df_numeric.drop(columns=['Survived'])
+    y = df_numeric['Survived']
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    model = RandomForestClassifier(
+        n_estimators=args.n_estimators, 
+        max_depth=args.max_depth, 
+        random_state=42
+    )
+    model.fit(X_train, y_train)
+    
+    y_pred = model.predict(X_test)
+    akurasi = accuracy_score(y_test, y_pred)
+    mlflow.log_metric("testing_accuracy_score", akurasi)
+    
+    print(f"✅ Model sukses dilatih! Akurasi Uji: {akurasi:.2%}")
